453ccdd28ec2daeacabb6ba7fcb50b9d9a0fc974
Andrew Brink <abrink27@gmail.com>
2016-07-11 19:30:19 +0000

    permissions added for enrollment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_currently_enrolled, only: [:show]
+
   def show
   end
 
   private
+
   helper_method :current_lesson
+
+  def require_currently_enrolled
+    if ! current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => "You must be enrolled to view the lesson"
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
