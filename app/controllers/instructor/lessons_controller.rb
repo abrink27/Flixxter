706098447817ea06bf58a6f9803047f47f0e21c8
Andrew Brink <abrink27@gmail.com>
2016-08-18 04:24:34 +0000

    Modal added for lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,15 +1,11 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
-  end
-
-  def new
-     @lesson = Lesson.new
   end
 
   def update
@@ -31,7 +27,7 @@ class Instructor::LessonsController < ApplicationController
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render text: 'Unauthorized', status: :unauthorized
+      render text: 'Unauthorized', status: :unauthorized
     end
   end
 
